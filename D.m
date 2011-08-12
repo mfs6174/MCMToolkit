494a1ec583bb9f871758afcac1b583be447a5078
Xijin Zhang <mfs6174@gmail.com>
2011-08-12 23:33:31 +0800

    数值解以及相轨线示例

--- D.m
+++ D.m
@@ -7,12 +7,18 @@
 function dy=fun(t,y)
 	dy=[t,y(2),5];
 
+%以上应当作为一个函数文件
+%求数值解
 ts=[0,100];
 y0=[1,2,3,5];
-[T,Y]=ode45('fun',ts,y0);
+[t,y]=ode45('fun',ts,y0);
+
+%画出相轨线
+plot(y(:,1),y(:,2));
+gird on;
 
 %解析解
-dsolve('方程等式 Dy D2y x','初值等式','x');
-[X,Y]=dsolve('方程1','方程2');
+%dsolve('方程等式 Dy D2y x','初值等式','x');
+%[X,Y]=dsolve('方程1','方程2');
 
 
