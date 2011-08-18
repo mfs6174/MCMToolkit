9e16b2d9987783300fbb735506fa3d216ae8414e
Xijin Zhang <mfs6174@gmail.com>
2011-08-19 00:14:23 +0800

    边缘程序和数据

--- 08B/myedge.m
+++ 08B/myedge.m
@@ -2,3 +2,15 @@ img=imread('datapic1.jpg');
 img=rgb2gray(img);
 [E,th,gv,gh]=EDGE(img,'sobel');
 figure,imshow(E);
+fid=fopen('data1.txt','w');
+fid2=fopen('data2.txt','w');
+[n,m]=size(E);
+ee=uint8(E);
+for i=1:n
+    fprintf(fid,'%d ',ee(i:i,:));
+    fprintf(fid,'\n');
+    fprintf(fid2,'%d',ee(i:i,:));
+    fprintf(fid2,'\n');
+end
+fclose(fid);
+
