cc772bfd495197a76cd2c6a10a648fba05843e26
shanzi <yun.er.run@gmail.com>
2011-08-27 17:45:30 +0800

    设定初值

--- 3/reg.m
+++ 3/reg.m
@@ -7,7 +7,8 @@ f4=fittype('y0*exp(r*t)+k/(1+exp(c-r*t))','independent',{'t'},'coefficients',{'r
 f5=fittype('y0*exp(r*t)+q*sin(w*t+o)','independent',{'t'},'coefficients',{'r','q','w','o','y0'});
 f6=fittype('q*sin(w*t+o)+k/(1+exp(c-r*t))','independent',{'t'},'coefficients',{'q','w','o','k','c','r'});
 
-x=(1:10)';
+n=1:9;
+x=n';
 [y(1,:),y(2,:),y(3,:),y(4,:)]=textread('data2.txt','%f%f%f%f');
 %f1=fittype('a+b*t','independent',{'t'},'coefficients',{'a','b'});
 for i=1:1
@@ -18,37 +19,39 @@ for i=1:1
         flag=1
     end
     mr=-10;
-    [fun,G]=fit(x,y(i,1:10)',f1);
+    [fun,G]=fit(x,y(i,n)',f1,'Startpoint',[200,30,1,200]);
         if G.rsquare>mr
             mfun=fun;mG=G;mr=G.rsquare;
         end
     
-    [fun,G]=fit(x,y(i,1:10)',f2);
+    [fun,G]=fit(x,y(i,n)',f2,'Startpoint',[200,30,100,1,1]);
         if G.rsquare>mr
             mfun=fun;mG=G;mr=G.rsquare;
         end
     
-    [fun,G]=fit(x,y(i,1:10)',f3);
+    [fun,G]=fit(x,y(i,n)',f3,'Startpoint',[200,30,1,0.01,0]);
         if G.rsquare>mr
             mfun=fun;mG=G;mr=G.rsquare;
         end
         
-    [fun,G]=fit(x,y(i,1:10)',f4);
+    [fun,G]=fit(x,y(i,n)',f4,'Startpoint',[1,1,1,200]);
         if G.rsquare>mr
             mfun=fun;mG=G;mr=G.rsquare;
         end
         
-    [fun,G]=fit(x,y(i,1:10)',f5);
+    [fun,G]=fit(x,y(i,n)',f5,'Startpoint',[1,1,0.1,0,200]);
         if G.rsquare>mr
             mfun=fun;mG=G;mr=G.rsquare;
         end
-        
-    [fun,G]=fit(x,y(i,1:10)',f6);
+  
+    if length(n)>6
+    [fun,G]=fit(x,y(i,n)',f6,'Startpoint',[1,0.1,0,1,1,1]);
         if G.rsquare>mr
             mfun=fun;mG=G;mr=G.rsquare;
         end
+    end
     i
     mfun
     mG
-    output_fig([x',11],y(i,:),mfun,'shanghai1');
+    output_fig(1:11,y(i,:),mfun,'shanghai1');
 end
