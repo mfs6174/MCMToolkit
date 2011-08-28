a4b1226f0e74ab1bbd0fd10974af4b255dbd8de9
shanzi <yun.er.run@gmail.com>
2011-08-28 14:05:20 +0800

    修正了苏州第四个数据的结果

--- 3/reg.m
+++ 3/reg.m
@@ -7,7 +7,7 @@ f4=fittype('y0*exp(r*t)+k/(1+exp(c-r*t))','independent',{'t'},'coefficients',{'r
 f5=fittype('y0*exp(r*t)+q*sin(w*t+o)','independent',{'t'},'coefficients',{'r','q','w','o','y0'});
 f6=fittype('q*sin(w*t+o)+k/(1+exp(c-r*t))','independent',{'t'},'coefficients',{'q','w','o','k','c','r'});
 
-s='shanghai';
+s='suzhou';
 [y(1,:),y(2,:),y(3,:),y(4,:)]=textread(sprintf('%s.txt',s),'%f%f%f%f');
 fid=fopen(sprintf('%s-result.txt',s),'w');
 %f1=fittype('a+b*t','independent',{'t'},'coefficients',{'a','b'});
@@ -67,7 +67,7 @@ for i=4:4
     if y(i,1)~=0
         output_fig(1:11,y(i,:),mfun,sprintf('%s%d',s,i),2000);
     else
-        output_fig([x(1:9)',11],y(i,2:11),mfun,sprintf('%s%d',s,i),2001);
+        output_fig(2:11,y(i,2:11),mfun,sprintf('%s%d',s,i),2001);
     end
 end
 fclose(fid);
